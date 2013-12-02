6440575dc9a458d7c9fd6a9c034699aba74e088a
William G Hatch <willghatch@gmail.com>
2013-12-02 14:30:05 -0700

    Add comment on nparts variable

--- globalvars.c
+++ globalvars.c
@@ -15,7 +15,9 @@ void initGlobals() {
     mdivision = 4;
     sdivPerDiv = 1;
     nmeasures = 12;
-    nparts = 4;
+    nparts = 4; // Ok, so this is a variable because I thought to make it configurable,
+        // but various parts I've since hard coded to be 4 parts.  So... unless I change
+        // that this needs to stay 4.
 
     totalSubdivisions = mdivision * sdivPerDiv * nmeasures;
 }
